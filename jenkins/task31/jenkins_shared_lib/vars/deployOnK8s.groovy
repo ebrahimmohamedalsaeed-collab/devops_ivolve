31f3e7d27cd61049fb8dfa4ced637a10420162c1
ebrahimmohamedalsaeed-collab <ebrahimmohamedalsaeed@gmail.com>
2025-11-05 11:02:32 +0200

    Update deployOnK8s.groovy

--- jenkins/task31/jenkins_shared_lib/vars/deployOnK8s.groovy
+++ jenkins/task31/jenkins_shared_lib/vars/deployOnK8s.groovy
@@ -1,8 +1,7 @@
 def call() {
     echo "Deploying application on Kubernetes..."
     sh '''
-    kubectl apply -f Jenkins_App/deployment.yaml
+    kubectl apply -f jenkins/task31/Jenkins_App/deployment.yaml
     kubectl get pods
     '''
 }
-
