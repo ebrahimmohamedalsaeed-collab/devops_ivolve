feef57b9de3f14b44a247ed2749f19093e46276f
ebrahimmohamedalsaeed-collab <ebrahimmohamedalsaeed@gmail.com>
2025-11-05 10:27:29 +0200

    Update buildImage.groovy

--- jenkins/task31/jenkins_shared_lib/vars/buildImage.groovy
+++ jenkins/task31/jenkins_shared_lib/vars/buildImage.groovy
@@ -1,6 +1,6 @@
 def call() {
     echo "Building Docker image..."
-    sh 'ls -la .'             // تأكد الملفات موجودة
-    sh 'docker build -t myapp:latest ./Jenkins_App'
+    // استخدم المسار الكامل داخل الـ repo
+    sh 'docker build -t myapp:latest jenkins/task31/Jenkins_App'
 }
 
