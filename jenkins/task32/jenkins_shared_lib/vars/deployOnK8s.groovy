59609b59446efed32c0c5c5978aa97e3c1a4cca4
ebrahimmohamedalsaeed-collab <ebrahimmohamedalsaeed@gmail.com>
2025-11-06 11:03:38 +0200

    Update deployOnK8s.groovy

--- jenkins/task32/jenkins_shared_lib/vars/deployOnK8s.groovy
+++ jenkins/task32/jenkins_shared_lib/vars/deployOnK8s.groovy
@@ -1,8 +1,15 @@
 def call(String imageTag) {
     def ns = env.BRANCH_NAME
     echo "Deploying ${imageTag} on namespace ${ns}"
+
+    def filePath = "jenkins/task32/Jenkins_App/deployment-template.yaml"
+    
+    if (!fileExists(filePath)) {
+        error "❌ File ${filePath} does not exist on this node!"
+    }
+
     sh """
-    sed 's|IMAGE_PLACEHOLDER|${imageTag}|' jenkins/task32/Jenkins_App/deployment-template.yaml > /tmp/deploy-${ns}.yaml
+    sed 's|IMAGE_PLACEHOLDER|${imageTag}|' ${filePath} > /tmp/deploy-${ns}.yaml
     kubectl apply -n ${ns} -f /tmp/deploy-${ns}.yaml
     kubectl get pods -n ${ns}
     """
