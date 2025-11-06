efd5eb6d9950689b6dc8725d8fe561ccaa688503
ebrahimmohamedalsaeed-collab <ebrahimmohamedalsaeed@gmail.com>
2025-11-06 10:29:38 +0200

    Update pushImage.groovy

--- jenkins/task32/jenkins_shared_lib/vars/pushImage.groovy
+++ jenkins/task32/jenkins_shared_lib/vars/pushImage.groovy
@@ -2,7 +2,7 @@ def call(String imageName) {
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
         sh """
         echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-        docker build -t ${imageName} .
+        docker build -t ${imageName} jenkins/task32/Jenkins_App
         docker push ${imageName}
         """
     }
